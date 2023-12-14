4f1998194eb31f1f322751d69a86bbd3aef26aaf
Fardeen <ahmad_fardeen@hotmail.com>
2023-12-14 12:52:39 -0500

    first package added

---
+++ src/firstPractice/praciticeOne.java
@@ -0,0 +1,14 @@
+package firstPractice;
+
+public class praciticeOne {
+
+	public static void main(String[] args) {
+		
+		int a = 10;
+		int b =22;
+		int c = a+b;
+		System.out.println(c);
+
+	}
+
+}
